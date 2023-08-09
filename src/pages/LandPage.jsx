aa8d2e683d0d7d615430ff5ae28306b0fa10c0d4
Tookim4 <tookim4@gmail.com>
2023-08-09 16:08:46 +0300

    the wait

--- src/pages/LandPage.jsx
+++ src/pages/LandPage.jsx
@@ -5,8 +5,10 @@ const LandPage = () => {
   return (
     <div>
       <div className="home_page">
-        <h1>Ideas</h1>
-        <p>simple one pager</p>
+        <h1 style={{fontSize:'6rem'}}>Hi there!!</h1>
+        <p>You probably are looking for Kimutai Too's website!</p>
+        <p>Its great you took your time to check it out, sadly, he's working to improve the site and make it even better.</p>
+        <p>Here's a hint,check in after a while and he'll be back</p>
       </div>
     </div>
   )
