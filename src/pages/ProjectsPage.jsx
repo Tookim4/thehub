6494bcac162d54a7501bbe1446e6a3b385d29b7a
Tookim4 <tookim4@gmail.com>
2023-12-30 09:15:07 +0300

    UI UPDATES

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -32,6 +32,44 @@ const ProjectsPage = () => {
               </div>
             </a>
 
+            <a target='_blank' href="https://afreative.com">
+              <div className="code-content">
+                  <div class="image-container">
+                    <img src={rickimg} alt="" className='project-logo' />
+                    </div>
+
+                    <div className="project-title">
+                      <h4 className="project-name">Afreative</h4>
+                      <hr />
+                      <p className="tech">REACTJS | JS | SASS | </p>
+                      <p className="year">2020</p>
+                  </div>
+
+                  <div className="hoverimg-div">
+                     <img src={hoverimg} alt="" className="hover-img" />
+                  </div>
+              </div>
+            </a>
+
+            <a target='_blank' href="https://rick-and-morty-api-sigma.vercel.app/">
+              <div className="code-content">
+                  <div class="image-container">
+                    <img src={rickimg} alt="" className='project-logo' />
+                    </div>
+
+                    <div className="project-title">
+                      <h4 className="project-name">Kratos Dungeon</h4>
+                      <hr />
+                      <p className="tech">REACTJS | JS | CSS </p>
+                      <p className="year">2020</p>
+                  </div>
+
+                  <div className="hoverimg-div">
+                     <img src={hoverimg} alt="" className="hover-img" />
+                  </div>
+              </div>
+            </a>
+
         </div>
 
 
