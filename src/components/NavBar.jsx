53986a74e753b3f8a0de2189776fe7fca8e52215
Tookim4 <tookim4@gmail.com>
2023-12-30 10:19:30 +0300

    Minor Update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,11 +9,12 @@ const NavBar = () => {
   return (
     <div className="main-nav">
       <div className="nav-links">
-        <Link to={'/'} className='main-links'>
-          <IoHome/>
-        </Link>
+      
         <Link to={'/pages/AboutsPage'} className='main-links'>
           ABOUT
+        </Link>
+        <Link to={'/'} className='main-links'>
+          <IoHome/>
         </Link>
         <Link to={'/pages/ProjectsPage'} className='main-links'>
           PROJECTS
